cb073630087644cac058e964d0cd7c6b095ab86f
RaulRoman <raulromanjr@gmail.com>
2020-11-30 19:54:36 -0500

    added sendgrid, WIP

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -2,7 +2,15 @@ class HomesController < ApplicationController
     def index
         # binding.pry
         pp params
+        @order = {
+            :name => 'First order',
+            :email => 'Fake@fake.com',
+            :address => '7187997878',
+            :phone => '7284892479',
+            :message => 'Your order is pending'
+        }
         @title = "Order Form"
+        OrderCompletedMailer.new_order_email(@order).deliver
     end
 end
 
--- app/mailers/order_completed_mailer.rb
+++ app/mailers/order_completed_mailer.rb
@@ -1,2 +1,11 @@
 class OrderCompletedMailer < ApplicationMailer
+
+    def new_order_email(order)
+        # @order = params[:order]
+        puts "* " * 15
+        puts "About to send email"
+        mail(to: 'supermomsweets@gmail.com', subject: 'You have a new Order!' )
+
+    end
+
 end
