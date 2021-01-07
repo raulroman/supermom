6d86efff0b11e2fcf7491c431effc7cdd8abef09
RaulRoman <raulromanjr@gmail.com>
2021-01-06 22:37:34 -0500

    working abbreviations

--- app/mailers/order_completed_mailer.rb
+++ app/mailers/order_completed_mailer.rb
@@ -1,7 +1,17 @@
 class OrderCompletedMailer < ApplicationMailer
 
     def new_order_email(order)
-        @abbreviated_names = {"hdccp" => "Half Dozen Chocolate Covered Pretzels","dccp" => "Dozen Chocolate Covered Pretzels", "hdcco" => "Half Dozen Chocolate Covered Oreos", "dcco" => "Dozen Chocolate Covered Oreos", "hdccrk" => "Half Dozen Chocolate Covered Rice Krispies", "dccrk" => "Dozen Chocolate Covered Rice Krispies", "onests" => "1 Sweet treats set", "fivests" => "5 Sweet treats set", "coconut_flan" => "coconut_flan" }
+        @abbreviated_names = {
+            "hdccp" => "Half Dozen Chocolate Covered Pretzels",
+            "dccp" => "Dozen Chocolate Covered Pretzels",
+            "hdcco" => "Half Dozen Chocolate Covered Oreos",
+            "dcco" => "Dozen Chocolate Covered Oreos",
+            "hdccrk" => "Half Dozen Chocolate Covered Rice Krispies",
+            "dccrk" => "Dozen Chocolate Covered Rice Krispies",
+            "onests" => "1 Sweet treats set",
+            "fivests" => "5 Sweet treats set",
+            "small_sofrito" => "Small Sofrito",
+            "large_sofrito" => "Large Sofrito"}
         @order = order
         puts "* " * 15
         pp @order
